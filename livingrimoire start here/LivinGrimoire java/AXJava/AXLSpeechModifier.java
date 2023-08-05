c2e06c8b509fca5d4d1fba69e93b73e52b8502a0
moti barski <yottaenkigo@yahoo.com>
2023-08-06 02:04:45 +0300

    ax speech modifier 060823
    modifies speech strings.

---
+++ livingrimoire start here/LivinGrimoire java/AXJava/AXLSpeechModifier.java
@@ -0,0 +1,22 @@
+package AXJava;
+
+import java.util.Hashtable;
+
+public class AXLSpeechModifier extends AXLHousing{
+    public Hashtable<String,String> dic;
+
+    public AXLSpeechModifier(Hashtable<String, String> dic) {
+        this.dic = dic;
+    }
+
+    @Override
+    public String decorate(String str1) {
+        String result = "";
+        String[] words = str1.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = dic.getOrDefault(words[i], words[i]);
+            result = result + " " + words[i];
+        }
+        return result;
+    }
+}

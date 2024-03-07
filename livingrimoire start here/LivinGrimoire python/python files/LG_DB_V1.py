7be2f18642ddba01339f89ee4634d6bbaad352b8
moti barski <yottaenkigo@yahoo.com>
2024-03-07 07:41:46 +0200

    LG database file 070324

--- livingrimoire start here/LivinGrimoire python/python files/LG_DB_V1.py
+++ livingrimoire start here/LivinGrimoire python/python files/LG_DB_V1.py
@@ -1,41 +1,52 @@
+import os
+
 import pandas as pd
+
+from LivinGrimoire23 import AbsDictionaryDB
 
 '''
 example usage code:
-from LG_DB_V1 import *  # database
-    # Save data
-    save_to_csv('my_database.csv', 'key', 'value')
-    save_to_csv('my_database.csv', 'apple', 'fruit')
-    save_to_csv('my_database.csv', 'carrot', 'vegetable')
-
-    # Read data
-    print(read_from_csv('my_database.csv', 'apple'))  # Output: 'fruit'
-    print(read_from_csv('my_database.csv', 'carrot'))  # Output: 'vegetable'
-    print(read_from_csv('my_database.csv', 'banana'))  # Output: None
-
-    # Check if data exists
-    print(check_data_exists('my_database.csv', 'apple'))  # Output: True
-    print(check_data_exists('my_database.csv', 'banana'))  # Output: False
+    db1: AbsDictionaryDB = LivinGrimoirePandaDB()
+    db1.save("test1", "working1")
+    db1.save("test2", "working2")
+    db1.save("test2", "working3")
+    print(db1.load("test1"))
+    print(db1.load("test2"))
+    print(db1.load("test3"))
 '''
 
 
-def save_to_csv(filename, key, value):
-    # Create a DataFrame with the given key and value
-    df = pd.DataFrame({'key': [key], 'value': [value]})
-    # Append to the existing CSV file or create a new one
-    df.to_csv(filename, mode='a', header=False, index=False)
+def save_to_txt(db_directory, key, value):
+    with open(f'{db_directory}/{key}.txt', mode="w") as file:
+        file.write(value)
 
 
-def read_from_csv(filename, key):
-    try:
-        df = pd.read_csv(filename)
-        value = df.loc[df['key'] == key, 'value'].iloc[0]
-        return value
-    except (FileNotFoundError, IndexError):
-        return None
+def read_from_txt(db_directory, key):
+    if not os.path.exists(f'{db_directory}/{key}.txt'):
+        return "null"
+    with open(f'{db_directory}/{key}.txt', 'r') as f:
+        return f.read()
 
 
-def check_data_exists(filename, key):
-    # Read the entire CSV file into a DataFrame
-    df = pd.read_csv(filename)
-    return key in df['key'].values
+def check_data_exists(db_directory, key):
+    return f'{db_directory}/{key}.txt'
+
+
+class LivinGrimoirePandaDB(AbsDictionaryDB):
+    def __init__(self, database_name: str = 'database'):
+        super().__init__()
+        self._db_name = database_name
+        if not os.path.exists(self._db_name):
+            os.makedirs(self._db_name)
+            print(f"Directory '{self._db_name}' created successfully.")
+        else:
+            print(f"Directory '{self._db_name}' already exists.")
+
+    def save(self, key: str, value: str):
+        save_to_txt(self._db_name, key, value)
+
+    def load(self, key: str) -> str:
+        return read_from_txt(self._db_name, key)
+
+    def dataExists(self, key: str) -> bool:
+        return check_data_exists(self._db_name, key)

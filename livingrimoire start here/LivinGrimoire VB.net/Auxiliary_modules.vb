5ac85e506c7223052dd32f4a0afe4206901da2ec
moti barski <yottaenkigo@yahoo.com>
2024-03-23 17:52:39 +0200

    axvb 230324

---
+++ livingrimoire start here/LivinGrimoire VB.net/Auxiliary_modules.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text.RegularExpressions
+Module Auxiliary_modules
+    Public Class RegexUtil
+        Public Function ExtractRegex(ByVal regexStr As String, ByVal ear As String) As String
+            Dim regex As New Regex(regexStr)
+            Dim match As Match = regex.Match(ear)
+
+            If match.Success Then
+                Return match.Value
+            Else
+                Return String.Empty
+            End If
+        End Function
+    End Class
+End Module

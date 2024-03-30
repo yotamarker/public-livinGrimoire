babdc8186e19731c97e8b06fe0c0f3093448c048
moti barski <yottaenkigo@yahoo.com>
2024-03-30 12:14:25 +0300

    vb.net incantation 0 experiment passed 300324
    flying colors with the DiTime skill

---
+++ livingrimoire start here/LivinGrimoire VB.net/skills.vb
@@ -0,0 +1,39 @@
+﻿Module skills
+    Public Class DiTime
+        Inherits DiSkillV2
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        ' Override
+        Public Overrides Sub Input(ear As String, skin As String, eye As String)
+            Select Case ear
+                Case "what is the date"
+                    Me.SetVerbatimAlg(4, $"{TimeUtils.getCurrentMonthDay()} {TimeUtils.getCurrentMonthName()} {TimeUtils.GetCurrentYear()}")
+                Case "what is the time"
+                    Me.SetVerbatimAlg(4, TimeUtils.getCurrentTimeStamp())
+                Case "honey bunny"
+                    Me.SetVerbatimAlg(4, "bunny honey")
+                Case "i am sleepy"
+                    Me.SetSimpleAlg("Chi… Chi knows it’s late. Sleep, sleep is good. When you sleep, you can dream. Dreams, dreams are nice. Tomorrow, lots to do. If sleep now, can do best tomorrow. So, let’s sleep. Good night… zzz…")
+                Case "which day is it"
+                    Me.SetVerbatimAlg(4, TimeUtils.getDayOfDWeek())
+                Case "good morning"
+                    Me.SetVerbatimAlg(4, $"good {TimeUtils.PartOfDay()}") ' fstring
+                Case "good night"
+                    Me.SetVerbatimAlg(4, $"good {TimeUtils.PartOfDay()}") ' fstring
+                Case "good afternoon"
+                    Me.SetVerbatimAlg(4, $"good {TimeUtils.PartOfDay()}") ' fstring
+                Case "good evening"
+                    Me.SetVerbatimAlg(4, $"good {TimeUtils.PartOfDay()}") ' fstring
+                Case "which month is it"
+                    Me.SetVerbatimAlg(4, TimeUtils.getCurrentMonthName())
+                Case "which year is it"
+                    Me.SetVerbatimAlg(4, $"{TimeUtils.GetCurrentYear}")
+                Case "what is your time zone"
+                    Me.SetVerbatimAlg(4, TimeUtils.GetLocalTimeZone())
+                Case "incantation 0"
+                    Me.SetVerbatimAlg(5, "fly", "bless of magic caster", "infinity wall", "magic ward holy", "life essen")
+            End Select
+        End Sub
+    End Class
+End Module

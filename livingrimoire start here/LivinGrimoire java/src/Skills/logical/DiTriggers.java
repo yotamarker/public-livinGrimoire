20cfd8742676e4b576c66ed010e167aba404e385
moti barski <yottaenkigo@yahoo.com>
2024-08-18 00:19:39 +0300

    DiTriggers 180824
    java version

---
+++ livingrimoire start here/LivinGrimoire java/src/Skills/logical/DiTriggers.java
@@ -0,0 +1,31 @@
+package Skills.logical;
+
+import Auxiliary_Modules.UniqueItemsPriorityQue;
+import LivinGrimoire.Brain;
+import LivinGrimoire.Chobits;
+import LivinGrimoire.DiSkillV2;
+
+public class DiTriggers extends DiSkillV2 {
+    private final UniqueItemsPriorityQue triggers = new UniqueItemsPriorityQue();
+    private boolean isRecording = false;
+    private final Brain brain;
+    private String newCMD = "";
+
+    public DiTriggers(Brain brain) {
+        this.brain = brain;
+    }
+
+    @Override
+    public void input(String ear, String skin, String eye) {
+        if(!isRecording){
+            if(!ear.isEmpty()){newCMD = ear;isRecording = true;} // not recording and hear something so record
+        }else {
+            if(!brain.getLogicChobitOutput().isEmpty()){triggers.add(newCMD);} // recording and output detected
+            isRecording = false;
+        }
+        // trigger output (can add alternative code to do this automatically)
+        if(ear.equals("random trigger")){
+            setSimpleAlg(triggers.getRNDElement());
+        }
+    }
+}

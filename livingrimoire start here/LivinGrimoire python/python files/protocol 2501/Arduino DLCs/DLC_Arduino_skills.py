563416f59c6b8138c5c185b116a56fa873b912a9
moti barski <yottaenkigo@yahoo.com>
2024-10-29 21:58:53 +0200

    Arduino DLC 2910242158

--- livingrimoire start here/LivinGrimoire python/python files/protocol 2501/Arduino DLCs/DLC_Arduino_skills.py
+++ livingrimoire start here/LivinGrimoire python/python files/protocol 2501/Arduino DLCs/DLC_Arduino_skills.py
@@ -4,40 +4,41 @@ import serial
 import time
 import atexit
 
-
 # terminal: pip install pyserial
+ser = serial.Serial('COM3', 9600, timeout=0.1)
+
+
+def close():
+    ser.close()
 
 
 class SerialReader:
-    def __init__(self, port='COM3', baud_rate=9600, timeout=0.1):
-        self.ser = serial.Serial(port, baud_rate, timeout=timeout)
-        atexit.register(self.close)  # Register the close method to be called on exit
 
-    def read_serial_data(self, num_readings=10) -> str:
-        for _ in range(num_readings):
-            if self.ser.in_waiting > 0:
-                line = self.ser.readline().decode('utf-8').rstrip()
-                line = RegexUtil.extractRegex("[-+]?[0-9]{1,3}", line)
-                # readings.append(line)
-                if len(line) > 0 and line != "0":
-                    return f'{int(line) - 20}'
-            time.sleep(1)  # Delay between readings
+    @staticmethod
+    def read_serial_data(num_readings=10) -> str:
+        try:
+            for _ in range(num_readings):
+                if ser.in_waiting > 0:
+                    line = ser.readline().decode('utf-8').rstrip()
+                    line = RegexUtil.extractRegex("[-+]?[0-9]{1,3}", line)
+                    if len(line) > 0 and line != "0":
+                        return f'{int(line) - 10}'
+                time.sleep(1)  # Delay between readings
+        except serial.SerialException as e:
+            return f"Error reading serial data: {e}"
         return "i do not know"
-
-    def close(self):
-        self.ser.close()
 
 
 class DiArduinoTemperature(Skill):
     def __init__(self):
         super().__init__()
-        self.reader = SerialReader()
 
     # Override
     def input(self, ear: str, skin: str, eye: str):
         if ear == "check temperature":
-            self.setVerbatimAlg(4, self.reader.read_serial_data())
+            self.setVerbatimAlg(4, SerialReader.read_serial_data())
 
 
 def add_DLC_skills(brain: Brain):
+    atexit.register(close)  # wrap up serial object when program closes
     brain.add_logical_skill(DiArduinoTemperature())
